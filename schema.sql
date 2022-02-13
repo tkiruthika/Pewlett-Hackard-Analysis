ded92e0f3c3576a0b7efedc055dc0f52b5059c23
tkiruthika <tkiruthika86@gmail.com>
2022-02-13 00:30:59 -0600

    Adding Schema.sql

--- schema.sql
+++ schema.sql
@@ -35,15 +35,6 @@ CREATE TABLE salaries (
 	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_emp (
-	emp_no INT NOT NULL,
-	dept_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no, dept_no)
-);
-
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
@@ -54,4 +45,12 @@ CREATE TABLE titles (
 	
 );
 
-SELECT * FROM departments;
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
